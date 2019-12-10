d31b5e01485928ab7f5164a270349879b8923e9c
camillejr <kamilazdybal@gmail.com>
2019-12-10 13:06:16 +0100

    Function clean-up, values are computed only when needed for the particular scaling - added for speed-up

--- functions/pre-post-processing/scale.m
+++ functions/pre-post-processing/scale.m
@@ -1,87 +1,125 @@
-% function [scaled_data, gamma] = scale(unscaled_data, uncentered_data, scal_crit, my_gamma)
-% 
-% This function provides a tool to scale the data according to several
-% criteria
+function [scaled_data, scalings, scaling_name_str] = scale(unscaled_data, uncentered_data, scal_crit, user_supplied_scalings)
+% This function scales the data set.
 %
-% INPUTS:
-% 
-% unscaled_data     = Data matrix to be scaled. Each row is an observation,
-%                     each column a variable
-% scal_crit         = Scaling criterion
+% Input:
+% ------------
+% - unscaled_data
+%         an unscaled data set.
 %
-% OUTPUT
+% - uncentered_data
+%         an uncentered and unscaled data set.
 %
-% scaled_data       = Matrix of scaled data
-% gamma             = Scaling parameter
+% - scal_crit
+%         selected scaling criteria. It has to be an integer between 0 and 9.
+%
+%         Available scalings:
+%
+%         0   NONE, no scaling is applied
+%         1   AUTO (STD), each variable is normalized by its standard
+%             deviation
+%         2   RANGE, each variable is normalized by its range
+%         3   PARETO, each variable is normalized by the square root of its standard
+%             deviation
+%         4   VAST, each variable is normalized by the standard deviation and
+%             coefficient of variation
+%         5   LEVEL, each variable is normalized by the mean of the data
+%         6   MAX, each variable is normalized by its maximum value
+%         7   KURTOSIS-1
+%         8   KURTOSIS-2
+%         9   KURTOSIS-3
+%
+% - user_supplied_scalings
+%         a user supplied vector of scalings that will be applied to the unscaled data set.
+%
+% Output:
+% ------------
+% - scaled_data
+%         a scaled data set.
+%
+% - scalings
+%         a vector of scalings that were applied to the unscaled data set.
+%
+% - scaling_name_str
+%         a string specifying the scaling option.
 
-function [scaled_data, gamma] = scale(unscaled_data, uncentered_data, scal_crit, my_gamma) 
-% 0) No scaling 
-% 1) Auto-scaling (STD), each variable is normalized by its standard
-% deviation 
-% 2) RANGE each variable is normalized by its range 
-% 3) PARETO, each variable is scaled by the square root of its standard
-% deviation  
-% 4) VAST, each variable is scaled by the standard deviation and
-% coefficient of variation 
-% 5) LEVEL, each variable is normalized by the mean of the data
-% 6) MAX, each variable is scaled by its maximum value
-
-% Definition of parameters
-[rows, columns] = size(unscaled_data);
+%% scale()
+% Checks:
+[n_obs, n_vars] = size(unscaled_data);
 a_tol = 1e-16;
-% Main values to estimate
-kurt_value = kurtosis(uncentered_data);
-mean_value = mean(uncentered_data, 1);
-min_value = min(uncentered_data, [], 1);
-max_value = max(abs(uncentered_data), [], 1);
-range_value = max_value - min_value;
-std_value = std(uncentered_data, 1, 1);
-pareto_value = std_value.^0.5;
-vast_value = std_value.^2 ./ (mean_value + a_tol);
-level_value = mean_value;
-vast_2 = std_value.^2 .* kurt_value.^2 ./ (mean_value + a_tol);
-vast_3 = std_value.^2 .* kurt_value.^2 ./ max_value;
-vast_4 = std_value.^2 .* kurt_value.^2 ./ range_value;
-% Choose scaling factors
-if ~exist('scal_crit', 'var') || isempty(scal_crit)
-    scal_crit = 1;
+
+if n_obs ~= size(uncentered_data, 1) || n_vars ~= size(unscaled_data, 2)
+  error('Dimensions of the uncentered data must be the same as the dimensions of the unscaled data.')
 end
-if ~exist('my_gamma', 'var') || isempty(my_gamma)
+
+if exist('user_supplied_scalings', 'var') || ~isempty(user_supplied_scalings)
+  if length(user_supplied_scalings) ~= n_vars
+    error('The size of the user supplied scalings vector must match the number of variables in the data set.')
+  end
+end
+
+% Find the vector of scalings:
+if ~exist('user_supplied_scalings', 'var') || isempty(user_supplied_scalings)
     switch scal_crit
         case 0
-            gamma = ones(1, columns);
+            % NONE
+            scalings = ones(1, n_vars);
             scaled_data = unscaled_data;
             return;
         case 1
-            gamma = std_value;
+            % AUTO
+            scalings = std(uncentered_data, 1, 1);
+            scaling_name_str = 'auto';
         case 2
-            gamma = range_value;
+            % RANGE
+            scalings = max(abs(uncentered_data), [], 1) - min(uncentered_data, [], 1);
+            scaling_name_str = 'range';
         case 3
-            gamma = pareto_value;
+            % PARETO
+            scalings = std(uncentered_data, 1, 1).^0.5;
+            scaling_name_str = 'pareto';
         case 4
-            gamma = vast_value;
+            % VAST
+            scalings = std(uncentered_data, 1, 1).^2 ./ (mean(uncentered_data, 1) + a_tol);
+            scaling_name_str = 'vast';
         case 5
-            gamma = level_value;
+            % LEVEL
+            scalings = mean(uncentered_data, 1);
+            scaling_name_str = 'level';
         case 6
-            gamma = max_value;
+            % MAX
+            scalings = max(abs(uncentered_data), [], 1);
+            scaling_name_str = 'max';
         case 7
-            gamma = vast_2;
+            % KURTOSIS-1
+            kurt_value = kurtosis(uncentered_data);
+            vast_2 = std_value.^2 .* kurt_value.^2 ./ (mean_value + a_tol);
+            scalings = vast_2;
+            scaling_name_str = 'std_kurt_1';
         case 8
-            gamma = vast_3;
+            % KURTOSIS-2
+            kurt_value = kurtosis(uncentered_data);
+            vast_3 = std_value.^2 .* kurt_value.^2 ./ max_value;
+            scalings = vast_3;
+            scaling_name_str = 'std_kurt_2';
         case 9
-            gamma = vast_4;
+            % KURTOSIS-3
+            kurt_value = kurtosis(uncentered_data);
+            vast_4 = std_value.^2 .* kurt_value.^2 ./ range_value;
+            scalings = vast_4;
+            scaling_name_str = 'std_kurt_3';
         otherwise
             error('Unknown scaling criterion');
     end
 else
-    % Provided externally
-    gamma = my_gamma;
+    % Use user supplied scaling:
+    scalings = user_supplied_scalings;
 end
-% Initialization and scaling
-scaled_data = zeros(rows, columns);
-for j = 1 : columns
-    scaled_data(:, j) = unscaled_data(:, j) / (gamma(j) + a_tol);
+
+% Scale the data set:
+scaled_data = zeros(n_obs, n_vars);
+
+for j = 1:1:n_vars
+    scaled_data(:, j) = unscaled_data(:, j) / (scalings(j) + a_tol);
 end
+
 end
-% save gamma.out gamma -ASCII -DOUBLE
-%save scaled_data scaled_data
