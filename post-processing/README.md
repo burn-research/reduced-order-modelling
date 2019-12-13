3e97a0ef3fbdba9f433c8905b85f73594408e214
camillejr <kamilazdybal@gmail.com>
2019-12-13 13:55:41 +0100

    Clean-up

--- post-processing/README.md
+++ post-processing/README.md
@@ -2,4 +2,16 @@
 
 ## Estimation of reconstruction errors
 
+Use the function `quality_of_reconstruction_measures()` to compute reconstruction errors between the original data `X` and the model fit to your data `F`.
+
+```matlab
+[r2, nrmse, mae, rmse] = quality_of_reconstruction_measures(X, F)
+```
+
 ## Data operations
+
+#### Rotation of factors
+
+Factors (eigenvectors or modes) coming from ROM technique can often be rotated to aid the interpretation.
+
+#### Procrustes analysis
--- post-processing/quality_of_reconstruction_measures.m
+++ post-processing/quality_of_reconstruction_measures.m
@@ -1,80 +1,73 @@
-function [r2, nrmse, mean_ab_e, rmse] = quality_of_reconstruction_measures(y, f, varargin)
-%% Description
-% Compute coefficient of determination of data fit model and RMSE
+function [r2, nrmse, mae, rmse] = quality_of_reconstruction_measures(X, F)
+% This function computes various quality of reconstruction measures such as
+% the coefficient of determination (R2), Normalized Root Mean Squared Error (NRMSE),
+% Mean Absolute Error (MAE) and Root Mean Squared Error (RMSE).
 %
-% [r2 nrmse] = rsquare(y,f)
-% [r2 nrmse] = rsquare(y,f,c)
+% Input:
+% ------------
+% - X
+%     the original data set.
 %
-% RSQUARE computes the coefficient of determination (R-square) value from
-% actual data Y and model data F. The code uses a general version of
-% R-square, based on comparing the variability of the estimation errors
-% with the variability of the original values. RSQUARE also outputs the
-% root mean squared error (RMSE) for the user's convenience.
+% - F
+%     a model fit to the original data set.
 %
-% Note: RSQUARE ignores comparisons involving NaN values.
+% Output:
+% ------------
+% - r2
+%     coefficient of determination.
 %
-% INPUTS
-%   Y       : Actual data
-%   F       : Model fit
+% - nrmse
+%     Normalized Root Mean Squared Error.
 %
-% OPTION
-%   C       : Constant term in model
-%             R-square may be a questionable measure of fit when no
-%             constant term is included in the model.
-%   [DEFAULT] TRUE : Use traditional R-square computation
-%            FALSE : Uses alternate R-square computation for model
-%                    without constant term [R2 = 1 - NORM(Y-F)/NORM(Y)]
+% - mae
+%     Mean Absolute Error.
 %
-% OUTPUT
-%   R2      : Coefficient of determination
-%   NRMSE   : Normalized Root mean squared error
-%   MAE     : Mean Absolute Error
-%   RMSE    : Root mean squared error
-%
+% - rmse
+%     Root Mean Squared Error.
 
-%% Input
-if isempty(varargin)
-    c = true;
-elseif length(varargin) > 1
-    error('Too many input arguments');
-elseif ~islogical(varargin{1})
-    error('C must be logical (TRUE||FALSE)');
-else
-    c = varargin{1};
-end
-% Compare inputs
-if ~all( size(y) == size(f) )
+%% quality_of_reconstruction_measures()
+% Checks:
+if ~all( size(X) == size(F) )
     error('Y and F must be the same size');
 end
 
-ydim = size(y,2);
+% Check for NaN:
+tmp = ~or(isnan(X),isnan(F));
+X = X(tmp);
+F = F(tmp);
+
+ydim = size(X,2);
+
 if ydim > 1
+
+    % Initialize measures:
     r2 = zeros(ydim, 1);
     nrmse = zeros(ydim, 1);
-    mean_ab_e = zeros(ydim, 1);
+    mae = zeros(ydim, 1);
     rmse = zeros(ydim, 1);
-    for ii = 1 : ydim
-        [r2(ii), nrmse(ii), mean_ab_e(ii), rmse(ii)] = r_square(y(:,ii), f(:,ii), c);
+
+    % Run this function in recurrence on each column of X and F:
+    for ii = 1:1:ydim
+        [r2(ii), nrmse(ii), mae(ii), rmse(ii)] = quality_of_reconstruction_measures(X(:,ii), F(:,ii));
     end
+
     return
 end
 
-%% Main
-% Check for NaN
-tmp = ~or(isnan(y),isnan(f));
-y = y(tmp);
-f = f(tmp);
-if c
-    r2 = max(0, 1 - sum((y(:) - f(:)).^2) / sum((y(:) - mean(y(:))).^2));
-else
-    r2 = 1 - sum((y(:) - f(:)).^2) / sum(y(:).^2);
-    if (r2 < 0)
-    % http://web.maths.unsw.edu.au/~adelle/Garvan/Assays/GoodnessOfFit.html
-        warning('Consider adding a constant term to your model') %#ok<WNTAG>
-        r2 = 0;
-    end
+% Coefficient of determination:
+r2 = 1 - sum((X(:) - F(:)).^2) / sum(X(:).^2);
+if (r2 < 0)
+    warning('Consider adding a constant term to your model.')
+    r2 = 0;
 end
-nrmse = sqrt(mean((y(:) - f(:)).^2)) / sqrt(mean(y.^2));
-mean_ab_e = mean(abs(y(:) - f(:))) / mean(abs(y));
-rmse = sqrt(mean((y(:) - f(:)).^2));
+
+% Normalized Root Mean Squared Error:
+nrmse = sqrt(mean((X(:) - F(:)).^2)) / sqrt(mean(X.^2));
+
+% Mean Absolute Error:
+mae = mean(abs(X():) - F(:))) / mean(abs(X));
+
+% Root Mean Squared Error:
+rmse = sqrt(mean((X(:) - F(:)).^2));
+
 end
