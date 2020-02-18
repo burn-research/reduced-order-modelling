660a5cdf5ee3bca029ce970f7d86601d887ebf9d
camillejr <kamilazdybal@gmail.com>
2020-02-18 10:28:22 -0700

    Added constant term in the model computation of R2

--- post-processing/quality_of_reconstruction_measures.m
+++ post-processing/quality_of_reconstruction_measures.m
@@ -1,4 +1,4 @@
-function [r2, nrmse, mae, rmse] = quality_of_reconstruction_measures(X, F)
+function [r2, nrmse, mae, rmse] = quality_of_reconstruction_measures(X, F, r2_constant)
 % This function computes various quality of reconstruction measures such as
 % the coefficient of determination (R2), Normalized Root Mean Squared Error (NRMSE),
 % Mean Absolute Error (MAE) and Root Mean Squared Error (RMSE).
@@ -10,6 +10,12 @@ function [r2, nrmse, mae, rmse] = quality_of_reconstruction_measures(X, F)
 %
 % - F
 %     a model fit to the original data set.
+%
+% - r2_constant
+%     optional Boolean specifying whether R2 computation
+%     according to constant term in the model:
+%     https://web.maths.unsw.edu.au/~adelle/Garvan/Assays/GoodnessOfFit.html
+%     If this value is not specified or set to false, a traditional R2 computation is performed.
 %
 % Output:
 % ------------
@@ -31,6 +37,10 @@ if ~all( size(X) == size(F) )
     error('Y and F must be the same size');
 end
 
+if ~exist('r2_constant', 'var') || isempty(r2_constant)
+    r2_constant = false;
+end
+
 ydim = size(X,2);
 
 if ydim > 1
@@ -43,21 +53,23 @@ if ydim > 1
 
     % Run this function in recurrence on each column of X and F:
     for ii = 1:1:ydim
-        [r2(ii), nrmse(ii), mae(ii), rmse(ii)] = quality_of_reconstruction_measures(X(:,ii), F(:,ii));
+        [r2(ii), nrmse(ii), mae(ii), rmse(ii)] = quality_of_reconstruction_measures(X(:,ii), F(:,ii), r2_constant);
     end
 
     return
 end
 
 % Coefficient of determination:
-r2 = max(0, (1 - sum((X(:) - F(:)).^2) / sum((X(:) - mean(X(:))).^2)));
-
-% Different way of computing:
-% r2 = 1 - sum((X(:) - F(:)).^2) / sum(X(:).^2);
-% if (r2 < 0)
-%     warning('Consider adding a constant term to your model.')
-%     r2 = 0;
-% end
+if r2_constant == false
+  r2 = (1 - sum((X(:) - F(:)).^2) / sum((X(:) - mean(X(:))).^2));
+else
+  % Computation according to constant term in the model:
+  r2 = 1 - sum((X(:) - F(:)).^2) / sum(X(:).^2);
+  if (r2 < 0)
+      warning('Consider adding a constant term to your model.')
+      r2 = 0;
+  end
+end
 
 % Normalized Root Mean Squared Error:
 nrmse = sqrt(mean((X(:) - F(:)).^2)) / sqrt(mean(X.^2));
